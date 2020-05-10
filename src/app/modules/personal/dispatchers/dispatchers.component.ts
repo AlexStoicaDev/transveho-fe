248cd0761b4fe824054acb6d3eaca89def4be5b1
Stoica <petrutstoica123@yahoo.com>
2020-05-10 16:02:27 +0300

    Create dispatchers component

---
+++ src/app/modules/personal/dispatchers/dispatchers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatchers',
+  templateUrl: './dispatchers.component.html',
+  styleUrls: ['./dispatchers.component.scss']
+})
+export class DispatchersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
