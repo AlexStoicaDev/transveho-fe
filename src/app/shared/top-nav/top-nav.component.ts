d42d97d492159ba2d154cca9256d2fff78033ebc
Stoica <petrutstoica123@yahoo.com>
2020-05-16 13:50:41 +0300

    Remove jquery from navigation bar

--- src/app/shared/top-nav/top-nav.component.ts
+++ src/app/shared/top-nav/top-nav.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  OnDestroy,
+  Renderer2
+} from '@angular/core';
 
-//TODO refactor component logic template and style
+//TODO refactor component logic template(naming) and style, fix styling for mobile view
 
 @Component({
   selector: 'top-nav',
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.scss']
 })
-export class TopNavComponent implements OnInit, OnDestroy {
-  showMenu = false;
-  selectedPage = '';
-
-  constructor(private renderer: Renderer2) {
-    //this.renderer.addClass(document.body, 'navigation-bar-padding');
-  }
-
-  ngOnInit(): void {}
-
-  showHeader() {
-    this.showMenu = !this.showMenu;
-  }
+export class TopNavComponent implements OnDestroy, AfterViewChecked {
+  constructor(private renderer: Renderer2) {}
 
   ngOnDestroy() {
     this.renderer.removeClass(document.body, 'modal-open');
   }
 
-  setSelectedPage(selectedPage: string) {
-    this.selectedPage = selectedPage;
+  setSelectorDivPosition() {
+    const activeLink = document.getElementsByClassName('active')[0];
+    if (activeLink) {
+      const selectedLinkCover = document.getElementsByClassName(
+        'hori-selector'
+      )[0] as HTMLElement;
+      selectedLinkCover.style.top =
+        (activeLink as HTMLElement).offsetTop + 'px';
+      selectedLinkCover.style.left =
+        (activeLink as HTMLElement).offsetLeft + 'px';
+      selectedLinkCover.style.height = activeLink.clientHeight + 1 + 'px';
+      selectedLinkCover.style.width = activeLink.clientWidth + 'px';
+    }
   }
 
-  createDriver() {}
+  ngAfterViewChecked(): void {
+    this.setSelectorDivPosition();
+  }
+
+  onResize($event: any) {
+    this.setSelectorDivPosition();
+  }
 }
